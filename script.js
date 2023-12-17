6fdf66684c1c99a24524ae20e9c36b5417c8c54d
Vicheka Oeun <oeunvicheka95@gmail.com>
2023-12-17 00:02:54 -0500

    made calculate function work

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ function divide(a, b) { return a / b };
 let num1, num2, op;
 // Selection
 function operation(num1, num2, op) {
-    if (op === "add") {
-        add(num1, num2);
+    if (op === "+") {
+        return add(num1, num2);
     }
-    else if (op === "subtract") {
-        subtract(num1, num2);
+    else if (op === "_") {
+        return subtract(num1, num2);
     }
-    else if (op === "multiply") {
-        multiply(num1, num2);
+    else if (op === "X") {
+        return multiply(num1, num2);
     }
-    else if (op === "divide") {
-        divide(num1, num2);
+    else if (op === "%") {
+        return divide(num1, num2);
     }
 };
 
@@ -54,6 +54,7 @@ for (let i = 0; i < buttonLabels.length; i++) {
 }
 
 // Click mechanic
+let operator;
 for (let i = 0; i < buttonLabels.length; i++) {
     const button = document.getElementById("button" + (i + 1));
 
@@ -66,8 +67,22 @@ for (let i = 0; i < buttonLabels.length; i++) {
             value.pop();
             Display(value);
         }
+        else if (button.textContent === "=") {
+            num2 = parseFloat(value.join('')); // Changes to float
+            result = operation(num1, num2, operator);
+            value = [result];
+            Display(value);
+            value = []; // Resets
+        }
+
+        else if (button.textContent === "X" || button.textContent === "_" || button.textContent === "+" || button.textContent === "%") {
+            operator = button.textContent;
+            num1 = parseFloat(value.join('')); // Changes to float
+            value = [];
+        }
         else {
             value.push(button.textContent);
+
             Display(value);
         }
         console.log(value); // Just for debugging purposes
@@ -78,6 +93,6 @@ for (let i = 0; i < buttonLabels.length; i++) {
 // Create Display
 let text;
 function Display(value) {
-    text = display.textContent = value.join('');
-    text.setAttribute("class", "text");
+    text = value.join('');
+    display.textContent = text;
 }
